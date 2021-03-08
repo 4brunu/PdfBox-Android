91badeb55c5ac71346e7916ae3a45ebc4a23ab4e
Bruno Coelho <4brunu@gmail.com>
2021-03-08 16:56:06 +0000

    Migrate pdfbox util to version 2.0.22

--- library/src/main/java/com/tom_roush/pdfbox/util/Hex.java
+++ library/src/main/java/com/tom_roush/pdfbox/util/Hex.java
@@ -17,11 +17,14 @@
 
 package com.tom_roush.pdfbox.util;
 
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+
 
 /**
  * Utility functions for hex encoding.
@@ -201,21 +204,27 @@ public final class Hex
         }
         catch (ClassNotFoundException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (IllegalAccessException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (IllegalArgumentException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (NoSuchMethodException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (SecurityException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (InvocationTargetException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         try
         {
@@ -226,22 +235,29 @@ public final class Hex
         }
         catch (ClassNotFoundException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (IllegalAccessException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (IllegalArgumentException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (NoSuchMethodException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (SecurityException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
         catch (InvocationTargetException ex)
         {
+            Log.d("PdfBox-Android", null, ex);
         }
+        Log.e("PdfBox-Android", "Can't decode base64 value, try adding javax.xml.bind:jaxb-api to your build");
         return new byte[0];
     }
 
@@ -271,6 +287,7 @@ public final class Hex
                 }
                 catch (NumberFormatException ex)
                 {
+                    Log.e("PdfBox-Android", "Can't parse " + hexByte + ", aborting decode", ex);
                     break;
                 }
                 i += 2;
